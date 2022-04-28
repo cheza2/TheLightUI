728c9ca8bbbdb601ba3a4f51ce235d86d6708f62
Gennadiy Golubenko <ya.golubneko77@gmail.com>
2022-04-28 15:13:50 +0300

    Initial Commit

---
+++ TheLightUI/TheLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheLightUIApp.swift
+//  TheLightUI
+//
+//  Created by Gen Gol on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
